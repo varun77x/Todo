9de82a2ad67fb4a884ead8d0b101f6759a11de23
Varun <varun77x@gmail.com>
2024-09-06 02:59:56 +0530

    initialize

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,33 +2,44 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([]);
+  const [text,setText] = useState("");
+  function addTodo() {
+    console.log(2);
+    }
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{
+      padding: "5%"
+    }}>
+
+      <input style={{
+
+        height: "40px",
+        width: "20%",
+        border: "1px solid black",
+        boxShadow: "10 10",
+        borderRadius: "5px",
+        backgroundColor: "lightgrey"
+
+
+      }} type="text" placeholder='Type your todo' value={todo} onChange={(e) => {
+        setTodo(...todo,e.target.value);
+      }}></input>
+
+      <button style={{
+        height: "40px",
+        borderRadius: "5px",
+        backgroundColor: "red"
+
+      }} onClick={addTodo}>Add Todo</button>
+
+
+    </div>
   )
 }
 
