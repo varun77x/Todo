5596abb7f33e039ae4155b4369c6c65f43905597
Varun <varun77x@gmail.com>
2024-09-08 02:03:25 +0530

    partial put implementation

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -16,5 +16,26 @@ router.post('/create', async (req, res) => {
     res.status(201).send({ message: 'Todo created' });
     console.log("todo created");
 })
+router.put('/update/:editId', async (req, res) => {
+    //no TRY CATCH , we go RAW
+    const id = req.params.editId;
+    console.log(id);
+    const textToUpdate = req.body.text;
+
+    const result = await Todo.updateOne(
+        { _id: id },
+        { $set: { todo: textToUpdate } }
+    );
+    if (result.matchedCount === 0) {
+        return res.status(404).json({ message: 'Todo not found' });
+    }
+
+    if (result.modifiedCount === 0) {
+        return res.status(200).json({ message: 'Todo was not modified' });
+
+    }
+    res.status(200).json({ message: "done" });
+}
+)
 
 module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   // const [todo, setTodo] = useState([]);
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [editId , setEditId] = useState(-1);
 
   async function findTodos() {
     const response = await axios.get('http://localhost:8100/todos/find');
@@ -23,58 +24,88 @@ function App() {
     }
   }
 
-
-    return (
-      <div style={{
-        padding: "5%"
-      }}>
-
-        <input style={{
-
-          height: "40px",
-          width: "20%",
-          border: "1px solid black",
-          boxShadow: "10 10",
-          borderRadius: "5px",
-          backgroundColor: "lightgrey"
-
-
-        }} type="text" placeholder='Type your todo' value={text} onChange={(e) => {
-          setText(e.target.value);
-        }}></input>
-        <br />
-
-        <button style={{
-          height: "40px",
-          borderRadius: "5px",
-          backgroundColor: "red"
-
-        }} onClick={addTodo}>Add Todo</button>
-        <br />
-
-        <button style={{
-          height: "40px",
-          borderRadius: "5px",
-          backgroundColor: "red",
-
-        }} onClick={findTodos}>All Todo</button>
-        <br />
-
-        {
-          todos.map((todo, index) =>
-            <div style={{
-              border:"2px solid black",
-              borderRadius:"10px",
-              padding:"5px",
-              margin:"2px",
-              width:"30%"
-            }} key={index}>{todo.todo}</div>
-          )
-        }
-
-
-      </div>
-    )
+  async function updateTodo(){
+    await axios.put(`http://localhost:8100/todos/update/${editId}`, { text });
+    setText("");
+    setEditId(-1);
   }
 
-  export default App;
+
+  return (
+    <div style={{
+      padding: "5%"
+    }}>
+
+      <input style={{
+
+        height: "40px",
+        width: "20%",
+        border: "1px solid black",
+        boxShadow: "10 10",
+        borderRadius: "5px",
+        backgroundColor: "lightgrey"
+
+
+      }} type="text" placeholder='Type your todo' value={text} onChange={(e) => {
+        setText(e.target.value);
+      }}></input>
+      <br />
+
+      <button style={{
+        height: "40px",
+        borderRadius: "5px",
+        backgroundColor: "red"
+
+      }} onClick={addTodo}>Add Todo</button>
+      <br />
+
+      <button style={{
+        height: "40px",
+        borderRadius: "5px",
+        backgroundColor: "red",
+
+      }} onClick={findTodos}>All Todo</button>
+      <br />
+
+      {
+        todos.map((todo, index) =>
+          <div style={{
+            border: "2px solid black",
+            borderRadius: "10px",
+            padding: "5px",
+            margin: "2px",
+            width: "30%"
+          }} key={todo._id}>
+            {todo._id === editId ?
+              (<>
+                <input type="text" value={text} onChange={(e) => {
+                  setText(e.target.value);
+                }}></input>
+                <button onClick = {updateTodo}>update</button>
+                <button onClick = {()=>{
+                  setEditId(-1);
+                }}>cancel</button>
+              </>)
+              :
+              (<>
+              {todo.todo}
+              <button onClick={() => {
+                setEditId(todo._id);
+                setText(todo.todo);
+                
+                
+              }}>edit todo</button>
+            
+              </>)
+            }
+            </div>
+            
+        )
+      }
+
+
+    </div>
+  )
+}
+
+export default App;
